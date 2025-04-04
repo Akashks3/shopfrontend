fe89a07d2286ed80a7359f82aa048ccd8523dd28
akash <155128336+Akashks3@users.noreply.github.com>
2025-04-04 13:07:33 +0530

    Add files via upload

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import BreadCrumb from "../components/BreadCrumb";
 import { HiOutlineArrowLeft } from "react-icons/hi";
 import Meta from "../components/Meta";
-import blog from "../images/blog-1.jpg";
+
 import Container from "../components/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { getABlog } from "../features/blogs/blogSlice";
@@ -32,7 +32,6 @@ const SingleBlog = () => {
                 <HiOutlineArrowLeft className="fs-4" /> Go back to Blogs
               </Link>
               <h3 className="title">{blogState?.title} </h3>
-              <img src={blog} className="img-fluid w-100 my-4" alt="blog" />
               <p
                 dangerouslySetInnerHTML={{ __html: blogState?.description }}
               ></p>
